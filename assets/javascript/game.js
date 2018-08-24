9e8eae1cffd12c1e3b5e9ac31d721e3b5d385a2e
Kathleen Doviken <kathleendoviken@gmail.com>
2018-08-24 08:10:49 -0500

    pretty sure this works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,26 +9,26 @@ var fighters = [
         name: 'Janeway',
         hp: 150,
         hpSpot: 'hp0',
-        fight: 8,
-        currentFight: 8,
-        hit: 20,
+        fight: 7,
+        currentFight: 7,
+        hit: 10,
         pic: 'assets/images/janeway.jpg'
     },
     {
         name: 'Borg Queen',
-        hp: 180,
+        hp: 160,
         hpSpot: 'hp1',
-        fight: 6,
-        currentFight: 6,
-        hit: 20,
+        fight: 3,
+        currentFight: 3,
+        hit: 14,
         pic: 'assets/images/borgqueen.jpg'
     },
     {
         name: 'Q',
-        hp: 120,
+        hp: 130,
         hpSpot: 'hp2',
-        fight: 8,
-        currentFight: 8,
+        fight: 7,
+        currentFight: 7,
         hit: 15,
         pic: 'assets/images/q.jpg'
     },
@@ -36,8 +36,8 @@ var fighters = [
         name: 'Seven of Nine',
         hp: 100,
         hpSpot: 'hp3',
-        fight: 14,
-        currentFight: 14,
+        fight: 20,
+        currentFight: 18,
         hit: 5,
         pic: 'assets/images/janeway.jpg'
     }
@@ -53,21 +53,18 @@ function reset() {
     fighterText.append("<h4>CHOOSE YOUR FIGHTER</h4>");
     for (let i = 0; i < fighters.length; i++) {
         currentHPSpot = fighters[i].hpSpot;
-        console.log(fighters[i].hp);
-        $("." + currentHPSpot).html(fighters[i].hp);
+        $("." + currentHPSpot).html("HP: " + fighters[i].hp);
     }
     // Select Player Character
     $(".fighter-pics").on('click', '.fighter', function (e) {
         if (playerFighter === undefined) {
             $(".player-fighter").append(this);
             playerFighter = $(this).attr('class').split(' ').pop();
-            console.log(playerFighter);
             fighterText.html("<h4>CHOOSE YOUR OPPONENT</h4>");
             // Select opponent
         } else if (opponent === undefined) {
             $(".computer-fighter").append(this);
             opponent = $(this).attr('class').split(' ').pop();
-            console.log(opponent);
         }
         // Player & Opponent selected: set up battle stage & call playRound
         if (playerFighter != undefined && opponent != undefined) {
@@ -99,28 +96,32 @@ function playRound() {
     let opponentHP = fighters[opponent].hp;
     // when attack button pressed:
     $('.attackbtn').unbind('click').bind('click', function () {
+        $('.battle-feedback').empty();
         // opponent gets hit by player attack
         opponentHP -= playerAttack;
-        $(opponenthpSpot).html(opponentHP);
+        $(opponenthpSpot).html("HP: " + opponentHP);
+        $('.battle-feedback').append("<p> You hit " + fighters[opponent].name + " for " + playerAttack + " damage! </p>");
         // player gets hit by opponent attack
         playerHP -= opponentAttack;
-        $(playerhpSpot).html(playerHP);
+        $(playerhpSpot).html("HP: " + playerHP);
+        $('.battle-feedback').append("<p>" + fighters[opponent].name + " hit you for " + opponentAttack + " damage! </p>");
         // player attack gets stronger
         playerAttack += playerFight;
         // if the player is dead:
         // note: if the player and opponent end with 0 or below HP, I consider the player to lose.
         // Player must end with HP > 0 to win. No wimps allowed to win.
         if (playerHP <= 0) {
+            $('.battle-feedback').empty();
             $(".choose-fighter").html("<h4 class='win-loss'> YOU LOSE!!! </h4 >");
             $('.fighter-pics').empty();
             $('.attack').empty();
             $('.attack').html('<button type="button" class="resetbtn rounded btn-lg" onClick="window.location.reload()">Reset</button>');
         // if opponent is dead...
         } else if (opponentHP <= 0) {
+            $('.battle-feedback').empty();
             opponentsKilled++;
             // ...and all other opponents have been defeated:
             if (opponentsKilled === 3 ) {
-                $(".computer-fighter").empty();
                 $(".choose-fighter").html("<h4 class='win-loss'> YOU WIN!!! </h4 >");
                 $('.attack').empty();
                 $('.attack').html('<button type="button" class="resetbtn rounded btn-lg" onClick="window.location.reload()">Reset</button>');
@@ -136,7 +137,6 @@ function playRound() {
                 $(".fighter-pics").on('click', '.fighter', function (e) {
                     $(".computer-fighter").append(this);
                     opponent = $(this).attr('class').split(' ').pop();
-                    console.log(opponent);
                     playRound();
                 });
             }
